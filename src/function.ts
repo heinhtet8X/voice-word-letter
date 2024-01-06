d46d0d5b40e887b1b4f40cc1f3709ccf05405996
Killer12344 <hh1716101@gmail.com>
2024-01-07 00:03:57 +0630

    first commit

---
+++ src/function.ts
@@ -0,0 +1,42 @@
+const generateString = async (length: number) => {
+    var chars: string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var charLength: number = chars.length;
+
+    var result: string = '';
+
+    for (var i = 0; i < length; i++) {
+        result += chars.charAt(Math.floor(Math.random() * charLength));
+    }
+
+    return result;
+}
+
+// need to defined html
+let randWord: HTMLElement = document.getElementById('randWord') as HTMLElement;
+let playBtn: ElementCSSInlineStyle = document.getElementById('playBtn') as ElementCSSInlineStyle;
+
+let code: string = '';
+
+let audio: any;
+
+const handleGenerate = (): void => {
+    generateString(10).then((result) => {
+        randWord.innerText = result;
+        code = result;
+        playBtn.style.display = 'inline-block';
+    });
+}
+
+const handlePlay = (): void => {
+    let letters: string[] = code.split("");
+
+    letters.map((letter: string, index: number) => {
+        setTimeout((): void => {
+            audio = new Audio(`assets/audio/${letter.toLocaleUpperCase()}.wav`);
+            audio.play();
+        }, index * 800);
+    });
+
+    playBtn.style.display = 'none';
+}
+
